b7adcb09c3acebf7aa681f5b49d2293d3736520a
Austin Kugler <44652750+austinpkugler@users.noreply.github.com>
2021-08-26 14:03:40 -0700

    Formatting

--- w01/w01.c
+++ w01/w01.c
@@ -1,41 +1,50 @@
+/*
+ * Name: Austin Kugler
+ * Assignment: W01
+ * Simple functionality for calculating the minimum distance betweeen
+ * any two elements in an array.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int MinDistance(int size, int arr[]);
 
+/*
+ * Accepts command line arguments as elements of an array.
+ */
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
-    {
-        printf("Enter at least two elements.\n");
-    }
-
-    int arr[argc - 1];
-
-    for (int i = 1; i < argc; ++i)
-    {
-        arr[i - 1] = atoi(argv[i]);
-    }
-
-    int dmin = MinDistance(argc - 1, arr);
-    printf("Minimum distance: %d\n", dmin);
-
-    return 0;
+   if (argc < 3)
+   {
+      printf("Enter at least two elements, separated by spaces.\n");
+      return 0;
+   }
+   int arr[argc - 1];
+   for (int i = 1; i < argc; ++i)
+   {
+      arr[i - 1] = atoi(argv[i]);
+   }
+   int dmin = MinDistance(argc - 1, arr);
+   printf("%d\n", dmin);
+   return 0;
 }
 
+/*
+ * Returns the minimum distance between any two elements in an array.
+ */
 int MinDistance(int size, int arr[])
 {
-    int dmin = -1;
-    for (int i = 0; i < size; ++i)
-    {
-        for (int j = 0; j < size; ++j)
-        {
-            int diff = abs(arr[i] - arr[j]);
-            if ((i != j && diff < dmin) || dmin == -1)
-            {
-                dmin = diff;
-            }
-        }
-    }
-    return dmin;
+   int dmin = abs(arr[0] - arr[1]);
+   for (int i = 0; i < size; ++i)
+   {
+      for (int j = 0; j < size; ++j)
+      {
+         int diff = abs(arr[i] - arr[j]);
+         if (i != j && diff < dmin)
+         {
+            dmin = diff;
+         }
+      }
+   }
+   return dmin;
 }
