c8cb98250a2636c8cfccfaef3589b3a8572bac07
austinpkugler <austinpkugler@gmail.com>
2021-09-03 07:40:51 -0700

    Formatting

--- w03/w03.c
+++ w03/w03.c
@@ -10,9 +10,7 @@
 
 int twoToN(int n);
 
-/*
- * Accepts command line argument to raise 2 to the power of.
- */
+// Accepts command line argument to raise 2 to the power of.
 int main(int argc, char *argv[])
 {
    if (argc != 2)
@@ -26,9 +24,7 @@ int main(int argc, char *argv[])
    return 0;
 }
 
-/*
- * Returns 2^n for the passed value of n.
- */
+// Returns 2^n for the passed value of n.
 int twoToN(int n)
 {
    if (n < 1)
