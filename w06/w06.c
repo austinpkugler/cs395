eb2138e061230c46ef142c0ca1766a4aa9dd22c1
austinpkugler <austinpkugler@gmail.com>
2021-09-20 09:41:22 -0700

    Finished

--- w06/w06.c
+++ w06/w06.c
@@ -2,21 +2,42 @@
  * Id: kugl5443
  *
  * Name: Austin Kugler
- * Assignment: W0
+ * Assignment: W05
  * 
+ * Solution for the restricted Tower of Hanoi problem.
  *********************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * Accepts command line arguments as elements of an array.
- */
+void printHanoi(int disks, char from, char temp, char to);
+
+// Accepts command line arguments as elements of an array.
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
-    {
-        printf("Enter at least two elements.\n");
-        return 0;
-    }
-    return 0;
+   if (argc != 2)
+   {
+      printf("Enter the number of disks to solve.\n");
+      return 0;
+   }
+
+   printHanoi(atoi(argv[1]), 'A', 'B', 'C');
+
+   return 0;
+}
+
+// Displays each step of the solution for the Tower of Hanoi.
+void printHanoi(int disks, char from, char temp, char to)
+{
+   if (disks == 1)
+   {
+      printf("Move %c TO %c\n", from, temp);
+      printf("Move %c TO %c\n", temp, to);
+      return;
+   }
+
+   printHanoi(disks - 1, from, temp, to);
+   printf("Move %c TO %c\n", from, temp);
+   printHanoi(disks - 1, to, temp, from);
+   printf("Move %c TO %c\n", temp, to);
+   printHanoi(disks - 1, from, temp, to);
 }
