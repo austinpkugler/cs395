475d0e6da1d7cc33176d413520cd962107f63418
austinpkugler <austinpkugler@gmail.com>
2021-09-20 09:18:27 -0700

    Formatting

--- w05/w05.c
+++ w05/w05.c
@@ -14,30 +14,30 @@ void printHanoi(int disks, char from, char to, char temp);
 // Accepts command line arguments as elements of an array.
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Enter the number of disks to solve.\n");
-        return 0;
-    }
+   if (argc != 2)
+   {
+      printf("Enter the number of disks to solve.\n");
+      return 0;
+   }
 
-    printHanoi(atoi(argv[1]), 'A', 'C', 'B');
+   printHanoi(atoi(argv[1]), 'A', 'C', 'B');
 
-    return 0;
+   return 0;
 }
 
 // Displays each step of the solution for the tower of Hanoi.
 void printHanoi(int disks, char from, char to, char temp)
 {
-    // n-1 disks A->B
-    // Last A disk A->C
-    // n-1 disks B->C
-    if (disks == 1)
-    {
-        printf("Move %c TO %c\n", from, to);
-        return;
-    }
+   // n-1 disks A->B
+   // Last A disk A->C
+   // n-1 disks B->C
+   if (disks == 1)
+   {
+      printf("Move %c TO %c\n", from, to);
+      return;
+   }
 
-    printHanoi(disks - 1, from, temp, to);
-    printf("Move %c TO %c\n", from, to);
-    printHanoi(disks - 1, temp, to, from);
+   printHanoi(disks - 1, from, temp, to);
+   printf("Move %c TO %c\n", from, to);
+   printHanoi(disks - 1, temp, to, from);
 }
