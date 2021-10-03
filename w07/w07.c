d105c8e3f6c9cd52f8f4601795922e67e52bba58
austinpkugler <austinpkugler@gmail.com>
2021-10-03 15:19:03 -0700

    Added assignment descriptions

---
+++ w07/w07.c
@@ -0,0 +1,43 @@
+/*********************************************
+ * Id: kugl5443
+ *
+ * Name: Austin Kugler
+ * Assignment: W07
+ * 
+ * Solution for the cyclic Tower of Hanoi problem.
+ *********************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void printHanoi(int disks, char from, char to, char temp);
+
+// Accepts command line arguments as elements of an array.
+int main(int argc, char *argv[])
+{
+   if (argc != 2)
+   {
+      printf("Enter the number of disks to solve.\n");
+      return 0;
+   }
+
+   printHanoi(atoi(argv[1]), 'A', 'C', 'B');
+
+   return 0;
+}
+
+// Displays each step of the solution for the tower of Hanoi.
+void printHanoi(int disks, char from, char to, char temp)
+{
+   // n-1 disks A->B
+   // Last A disk A->C
+   // n-1 disks B->C
+   if (disks == 1)
+   {
+      printf("disks=1: Move %c TO %c\n", from, to);
+      return;
+   }
+
+   printHanoi(disks - 1, from, temp, to);
+   printf("Move %c TO %c\n", from, to);
+   printHanoi(disks - 1, temp, to, from);
+}
---
+++ w21/w21.c
@@ -0,0 +1,70 @@
+/*********************************************
+ * Id: kugl5443
+ *
+ * Name: Austin Kugler
+ * Assignment: W07
+ * 
+ * Solution for the cyclic Tower of Hanoi problem.
+ *********************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printPermutations(int n);
+void swap(char *a, char *b);
+void permute(char *integers, int start, int end);
+
+// Accepts command line arguments as elements of an array.
+int main(int argc, char *argv[])
+{
+   if (argc != 2)
+   {
+      printf("Enter the number to display permutations for.\n");
+      return 0;
+   }
+
+   printPermutations(atoi(argv[1]));
+
+   return 0;
+}
+
+// Displays every permutation of numbers <= an integer n.
+void printPermutations(int n)
+{
+   char integers[n];
+   int i;
+   for (i = 1; i < n + 1; i++)
+   {
+      integers[i - 1] = i + '0';
+   }
+
+   permute(integers, 0, n - 1);
+}
+
+// Swaps two elements.
+void swap(char *a, char *b)
+{
+   char temp;
+   temp = *a;
+   *a = *b;
+   *b = temp;
+}
+
+// Recursive function to print every permutation of an integer.
+void permute(char *integers, int start, int end)
+{
+   if (start == end)
+   {
+      printf("%s\n", integers);
+   }
+   else
+   {
+      int i;
+      for (i = start; i <= end; i++)
+      {
+         swap((integers + start), (integers + i));
+         permute(integers, start + 1, end);
+         swap((integers + start), (integers + i));
+      }
+   }
+}
