89111ad4eb1d2d3b52aaffda6645d585e95c5bfc
austinpkugler <austinpkugler@gmail.com>
2021-10-05 16:50:41 -0700

    Implemented algorithm from assignment

--- w04/w04.c
+++ w04/w04.c
@@ -1,62 +1,108 @@
+// /*********************************************
+//  * Id: kugl5443
+//  *
+//  * Name: Austin Kugler
+//  * Assignment: W04
+//  * Uses Gaussian elimination to solve a system of linear equations.
+//  *********************************************/
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define SIZE 10
+void printMatrix(float matrix[], int n);
+void GE(float matrix[], int n);
 
-int main()
+// Accepts command line arguments for matrix values.
+int main(int argc, char *argv[])
 {
-   float a[SIZE][SIZE], x[SIZE], ratio;
-   int i, j, k, n;
-
-   /* Inputs */
-   /* 1. Reading number of unknowns */
-   printf("Enter number of unknowns: ");
-   scanf("%d", &n);
-   /* 2. Reading Augmented Matrix */
-   for (i = 1; i <= n; i++)
+   if (argc < 3)
    {
-      for (j = 1; j <= n + 1; j++)
-      {
-         printf("a[%d][%d] = ", i, j);
-         scanf("%f", &a[i][j]);
-      }
+      printf("Enter the number of equations followed by their elements.\n");
+      return 0;
    }
-   /* Applying Gauss Elimination */
-   for (i = 1; i <= n - 1; i++)
-   {
-      if (a[i][i] == 0.0)
-      {
-         printf("Mathematical Error!");
-         exit(0);
-      }
-      for (j = i + 1; j <= n; j++)
-      {
-         ratio = a[j][i] / a[i][i];
 
-         for (k = 1; k <= n + 1; k++)
+   int n = atoi(argv[1]);
+
+   float matrix[n * (n + 1)];
+   int i;
+   for (i = 2; i < argc; i++)
+   {
+      matrix[i - 2] = atof(argv[i]);
+   }
+
+   GE(matrix, n);
+
+   return 0;
+}
+
+// Prints an array in matrix form.
+void printMatrix(float matrix[], int n)
+{
+   int i;
+   for (i = 0; i < n * (n + 1); i++)
+   {
+      if (i && i % (n + 1) == 0)
+      {
+         printf("\n");
+      }
+      printf("%.2f ", matrix[i]);
+   }
+   printf("\n");
+}
+
+// Performs Gaussian Elimination on a matrix.
+void GE(float matrix[], int n)
+{
+   float A[n][n + 1];
+   int row = 0, col = 0, offset = 0;
+
+   while (row < n)
+   {
+      for (offset = row * (n + 1); offset < row * (n + 1) + (n + 1); offset++)
+      {
+         col = offset - (row * (n + 1));
+         A[row][col] = matrix[offset];
+      }
+      row++;
+   }
+
+   // Print the matrix
+   int y, z;
+   for (y = 0; y < n; y++)
+   {
+      for (z = 0; z < n + 1; z++)
+      {
+         printf("%.2f ", A[y][z]);
+      }
+      printf("\n");
+   }
+   printf("\n");
+
+   // Gaussian elimination
+   float tempji;
+
+   int i, j, k;
+   for (i = 0; i < n - 2; i++)
+   {
+      for (j = i + 1; j < n - 1; j++)
+      {
+         tempji = A[j][i];
+         for (k = i; k < n; k++)
          {
-            a[j][k] = a[j][k] - ratio * a[i][k];
+            A[j][k] = A[j][k] - (A[i][k] * tempji / A[i][i]);
          }
-      }
-   }
-   /* Obtaining Solution by Back Subsitution */
-   x[n] = a[n][n + 1] / a[n][n];
 
-   for (i = n - 1; i >= 1; i--)
-   {
-      x[i] = a[i][n + 1];
-      for (j = i + 1; j <= n; j++)
-      {
-         x[i] = x[i] - a[i][j] * x[j];
+         // Print the matrix
+         int y, z;
+         for (y = 0; y < n; y++)
+         {
+            for (z = 0; z < n + 1; z++)
+            {
+               printf("%.2f ", A[y][z]);
+            }
+            printf("\n");
+         }
+         printf("\n");
       }
-      x[i] = x[i] / a[i][i];
    }
-   /* Displaying Solution */
-   printf("\nSolution:\n");
-   for (i = 1; i <= n; i++)
-   {
-      printf("x[%d] = %0.3f\n", i, x[i]);
-   }
-   return (0);
 }
